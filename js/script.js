75f93df5bc15085b105256a6d7008dd4e5d862bf
Martí Pardo <57571130+Mapaor4@users.noreply.github.com>
2025-02-22 23:20:48 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,22 +4,21 @@ const colors = ["#3CC157", "#2AA7FF", "#1B1B1B", "#FCBC0F", "#F85F36"];
 const numBalls = 50;
 const balls = [];
 
-// Utilitzar l'etiqueta <main> com a contenidor de les boletes
-const main = document.querySelector('main');
-const mainRect = main.getBoundingClientRect(); // Obtenir la mida del main
+// Utilitzar el contenidor .ball-container per les boletes
+const ballContainer = document.querySelector('.ball-container');
 
 for (let i = 0; i < numBalls; i++) {
   let ball = document.createElement("div");
   ball.classList.add("ball");
   ball.style.background = colors[Math.floor(Math.random() * colors.length)];
-  ball.style.left = `${Math.floor(Math.random() * mainRect.width)}px`; // Posicionar dins del main
-  ball.style.top = `${Math.floor(Math.random() * mainRect.height)}px`; // Posicionar dins del main
+  ball.style.left = `${Math.floor(Math.random() * 100)}vw`; // Posicionar dins del viewport
+  ball.style.top = `${Math.floor(Math.random() * 100)}vh`; // Posicionar dins del viewport
   ball.style.transform = `scale(${Math.random()})`;
   ball.style.width = `${Math.random()}em`;
   ball.style.height = ball.style.width;
   
   balls.push(ball);
-  main.append(ball);
+  ballContainer.append(ball);
 }
 
 // Keyframes
