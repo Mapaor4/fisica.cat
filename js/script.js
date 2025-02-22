277233c5d8b3fd7675acc903309e6f8c26107e8c
Martí Pardo <57571130+Mapaor4@users.noreply.github.com>
2025-02-22 23:51:30 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ for (let i = 0; i < numBalls; i++) {
   ball.classList.add("ball");
   ball.style.background = colors[Math.floor(Math.random() * colors.length)];
   ball.style.left = `${Math.floor(Math.random() * 100)}vw`;
-  ball.style.top = `${Math.floor(Math.random() * 100)}vh`;
+  ball.style.top = `${Math.floor(Math.random() * 115)}vh`;
   ball.style.transform = `scale(${Math.random()})`;
   ball.style.width = `${Math.random()}em`;
   ball.style.height = ball.style.width;
