5fd01558afbd73f8edaa2557583b923cc99b400c
Martí Pardo <57571130+Mapaor4@users.noreply.github.com>
2025-02-22 23:36:13 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,21 +4,18 @@ const colors = ["#3CC157", "#2AA7FF", "#1B1B1B", "#FCBC0F", "#F85F36"];
 const numBalls = 50;
 const balls = [];
 
-// Utilitzar el contenidor .ball-container per les boletes
-const ballContainer = document.querySelector('.ball-container');
-
 for (let i = 0; i < numBalls; i++) {
   let ball = document.createElement("div");
   ball.classList.add("ball");
   ball.style.background = colors[Math.floor(Math.random() * colors.length)];
-  ball.style.left = `${Math.floor(Math.random() * 100)}vw`; // Posicionar dins del viewport
-  ball.style.top = `${Math.floor(Math.random() * 100)}vh`; // Posicionar dins del viewport
+  ball.style.left = `${Math.floor(Math.random() * 100)}vw`;
+  ball.style.top = `${Math.floor(Math.random() * 100)}vh`;
   ball.style.transform = `scale(${Math.random()})`;
   ball.style.width = `${Math.random()}em`;
   ball.style.height = ball.style.width;
   
   balls.push(ball);
-  ballContainer.append(ball);
+  document.body.append(ball);
 }
 
 // Keyframes
